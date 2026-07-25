34d3c8d525b20e5171e53a933c9521d51407b04a
cathood0 <santiagowmar@gmail.com>
2026-07-25 02:15:06 -0400

    fix: most of issues with deprecated members and missing ones

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -3,7 +3,6 @@
 // (Research ▸ README) is selected/visible on startup.
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:example/main.dart';
